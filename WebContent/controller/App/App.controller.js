37e6ace8efde286ec4a2261982c46bd9afb29361
Marco Distrutti <marco.distrutti@gmail.com>
2017-06-08 18:24:12 +0200

    Step 7: JSON Model
    Data binding and first setModel introduction

--- WebContent/controller/App/App.controller.js
+++ WebContent/controller/App/App.controller.js
@@ -1,16 +1,28 @@
 sap.ui.define([
    "sap/ui/core/mvc/Controller",
-   "sap/m/MessageToast"
+   "sap/m/MessageToast",
+   "sap/ui/model/json/JSONModel"
 ], 
 
-function (Controller, MessageToast) {
+function (Controller, MessageToast, JSONModel) {
 	"use strict";
    
 	return Controller.extend("sap.ui.walkthrough.controller.App.App", {
-   
 		/**
 		* Controller Events
 		*/
+		
+		onInit : function () {
+			var oData = {
+	            recipient : {
+	            	name : "World"
+	            }
+			};
+			var oModel = new JSONModel(oData);
+			
+			this.getView().setModel(oModel);
+      },
+		
 		onShowHello : function () {
 			MessageToast.show("Hello world with Message Toast.");
 		}
